138b10a37987ece207f29f6d32c951c325d7450b
SpiritAshes <92069795+SpiritAshes@users.noreply.github.com>
2026-03-03 20:22:31 +0800

    Update extract_aachen.py

--- eval_Aachen/extract_aachen.py
+++ eval_Aachen/extract_aachen.py
@@ -1,4 +1,10 @@
+import sys
 import os
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+parent_dir = os.path.dirname(current_dir)
+sys.path.insert(0, parent_dir) 
+
 import glob
 import yaml
 import torch
